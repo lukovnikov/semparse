6460aee12e92e388d7a2f8771a0e7c78fb9f7215
lukovnikov <lukovnikov@outlook.com>
2019-02-06 15:14:02 +0100

    added standard seq2seq semantic parsing

---
+++ semparse/seq2seq_standard.py
@@ -0,0 +1,180 @@
+import qelos as q
+import torch
+from functools import partial
+import phraseatt
+import numpy as np
+
+
+class EncDec(torch.nn.Module):
+    def __init__(self, inpemb, enc, dec, **kw):
+        super(EncDec, self).__init__(**kw)
+        self.inpemb, self.enc, self.dec = inpemb, enc, dec
+
+    def forward(self, inpseq, outseq):
+        inpemb, ctx_mask = self.inpemb(inpseq)
+        ctx = self.enc(inpemb, mask=ctx_mask)
+        if ctx_mask is not None and ctx_mask.size(1) > ctx.size(1):
+            ctx_mask = ctx_mask[:, :ctx.size(1)]
+        outprobs = self.dec(outseq, ctx=ctx, ctx_mask=ctx_mask)
+        return outprobs
+
+
+class Test_EncDec(torch.nn.Module):
+    def __init__(self, inpemb, enc, dec, **kw):
+        super(Test_EncDec, self).__init__(**kw)
+        self.inpemb, self.enc, self.dec = inpemb, enc, dec
+
+    def forward(self, inpseq, outseq):  # (batsize, inpseqlen), (batsize, outseqlen)
+        inpemb, ctx_mask = self.inpemb(inpseq)
+        ctx = self.enc(inpemb, mask=ctx_mask)
+        if ctx_mask is not None and ctx_mask.size(1) > ctx.size(1):
+            ctx_mask = ctx_mask[:, :ctx.size(1)]
+        _outseq = outseq[:, 0]
+        outprobs = self.dec(_outseq, ctx=ctx, ctx_mask=ctx_mask)
+        outprobs = outprobs[:, :outseq.size(1)]
+        return outprobs
+
+
+def gen_datasets(which="geo"):
+    pprefix = "../data/"
+    if which == "geo":
+        pprefix = pprefix + "geoqueries/"
+        trainp = pprefix + "train.txt"
+        validp = pprefix + "test.txt"
+        testp = pprefix + "test.txt"
+    elif which == "atis":
+        pprefix += "atis/"
+        trainp = pprefix + "train.txt"
+        validp = pprefix + "dev.txt"
+        testp = pprefix + "test.txt"
+    elif which == "jobs":
+        pprefix += "jobqueries"
+        trainp = pprefix + "train.txt"
+        validp = pprefix + "test.txt"
+        testp = pprefix + "test.txt"
+    else:
+        raise q.SumTingWongException("unknown dataset")
+
+    nlsm = q.StringMatrix()
+    nlsm.tokenize = lambda x: x.split()
+    flsm = q.StringMatrix()
+    flsm.tokenize = lambda x: x.split()
+    devstart, teststart, i = 0, 0, 0
+    with open(trainp) as tf, open(validp) as vf, open(testp) as xf:
+        for line in tf:
+            line_nl, line_fl = line.strip().split("\t")
+            nlsm.add(line_nl)
+            flsm.add(line_fl)
+            i += 1
+        devstart = i
+        for line in vf:
+            line_nl, line_fl = line.strip().split("\t")
+            nlsm.add(line_nl)
+            flsm.add(line_fl)
+            i += 1
+        teststart = i
+        for line in xf:
+            line_nl, line_fl = line.strip().split("\t")
+            nlsm.add(line_nl)
+            flsm.add(line_fl)
+            i += 1
+    nlsm.finalize()
+    flsm.finalize()
+
+    nlmat = torch.tensor(nlsm.matrix).long()
+    flmat = torch.tensor(flsm.matrix).long()
+    gold = torch.tensor(flsm.matrix[:, 1:]).long()
+    gold = torch.cat([gold, torch.zeros_like(gold[:, 0:1])], 1)
+    tds = torch.utils.data.TensorDataset(nlmat[:devstart], flmat[:devstart], gold[:devstart])
+    vds = torch.utils.data.TensorDataset(nlmat[devstart:teststart], flmat[devstart:teststart], gold[devstart:teststart])
+    xds = torch.utils.data.TensorDataset(nlmat[teststart:], flmat[teststart:], gold[teststart:])
+    return (tds, vds, xds), nlsm.D, flsm.D
+
+
+def run_normal(lr=0.001,
+        gradnorm=1.,
+        batsize=10,
+        epochs=150,
+        embdim=50,
+        encdim=100,
+        numlayer=2,
+        cuda=False,
+        gpu=0,
+        which="geo"):
+    tt = q.ticktock("script")
+    tt.msg("running normal att")
+    device = torch.device("cpu")
+    if cuda:
+        device = torch.device("cuda", gpu)
+
+    # region data
+    tt.tick("generating data")
+    # dss, D = gen_sort_data(seqlen=seqlen, numvoc=numvoc, numex=numex, prepend_inp=False)
+    dss, nlD, flD = gen_datasets(which=which)
+    tloader, vloader, xloader = [torch.utils.data.DataLoader(ds, batch_size=batsize, shuffle=True) for ds in dss]
+    seqlen = len(dss[0][0][1])
+    tt.tock("data generated")
+    # endregion
+
+    # region model
+    tt.tick("building model")
+    # source side
+    inpemb = q.WordEmb(embdim, worddic=nlD)
+    encdims = [encdim] * numlayer
+    encoder = q.LSTMEncoder(embdim, *encdims, bidir=True)
+
+    # target side
+    decemb = q.WordEmb(embdim, worddic=flD)
+    decinpdim = embdim
+    decdims = [decinpdim] + [encdim] * numlayer
+    dec_core = torch.nn.Sequential(
+        *[q.rnn.LSTMCell(decdims[i-1], decdims[i]) for i in range(1, len(decdims))]
+    )
+    att = phraseatt.model.FwdAttention(qrydim=encdim, ctxdim=encdim*2, encdim=encdim)
+    out = torch.nn.Sequential(
+        q.WordLinout(encdim+encdim*2, worddic=flD),
+        # torch.nn.Softmax(-1)
+    )
+    deccell = q.rnn.DecoderCell(emb=decemb, core=dec_core,
+                               att=att, out=out)
+    train_dec = q.TFDecoder(deccell)
+    test_dec = q.FreeDecoder(deccell, maxtime=seqlen+10)
+    train_encdec = EncDec(inpemb, encoder, train_dec)
+    test_encdec = Test_EncDec(inpemb, encoder, test_dec)
+
+    train_encdec.to(device)
+    test_encdec.to(device)
+    tt.tock("built model")
+    # endregion
+
+    # region training
+    # losses:
+    ce = q.loss.CELoss(mode="logits", ignore_index=0)
+    acc = q.loss.SeqAccuracy(ignore_index=0)
+    elemacc = q.loss.SeqElemAccuracy(ignore_index=0)
+    # optim
+    optim = torch.optim.Adam(train_encdec.parameters(), lr=lr)
+    clipgradnorm = lambda: torch.nn.utils.clip_grad_norm(train_encdec.parameters(), max_norm=gradnorm)
+    # lööps
+    batchloop = partial(q.train_batch, on_before_optim_step=[clipgradnorm])
+    trainloop = partial(q.train_epoch, model=train_encdec, dataloader=tloader, optim=optim, device=device,
+                        losses=[q.LossWrapper(ce), q.LossWrapper(acc), q.LossWrapper(elemacc)],
+                        print_every_batch=False, _train_batch=batchloop)
+    validloop = partial(q.test_epoch, model=test_encdec, dataloader=vloader, device=device,
+                        losses=[q.LossWrapper(ce), q.LossWrapper(acc), q.LossWrapper(elemacc)],
+                        print_every_batch=False)
+
+    tt.tick("training")
+    q.run_training(trainloop, validloop, max_epochs=epochs)
+    tt.tock("trained")
+
+    tt.tick("testing")
+    test_results = validloop(dataloader=xloader)
+    print("Test results: {}".format(test_results))
+    tt.tock("tested")
+    # endregion
+    tt.msg("done")
+
+
+if __name__ == '__main__':
+    q.argprun(run_normal)
