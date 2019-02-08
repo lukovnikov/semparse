29f0cfb1ff756f42e4f3b9e73c611f8fdff6bf94
lukovnikov <lukovnikov@outlook.com>
2019-02-08 17:17:53 +0100

    combo rel att

--- semparse/seq2seq_standard.py
+++ semparse/seq2seq_standard.py
@@ -235,7 +235,7 @@ def run_relatt(lr=0.001,
     dec_core = torch.nn.Sequential(
         *[q.rnn.LSTMCell(decdims[i-1], decdims[i], dropout_in=dropout) for i in range(1, len(decdims))]
     )
-    att = phraseatt.model.RelAttention(ctxdim=encdim, vecdim=encdim)
+    att = phraseatt.model.ComboRelAttention(ctxdim=encdim, vecdim=encdim)
     out = torch.nn.Sequential(
         q.WordLinout(encdim+encdim, worddic=flD),
         # torch.nn.Softmax(-1)
